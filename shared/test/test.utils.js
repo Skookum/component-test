195220af07c1a0b678cde1b0bc41051f53f5ffe3
Hunter Loftis <hunter@hunterloftis.com>
2012-10-26 13:50:41 -0400

    fixed tests

--- shared/test/test.utils.js
+++ shared/test/test.utils.js
@@ -2,7 +2,7 @@ module.exports = {
   startApp: function(port) {
     var config = require('../../config.json');
     config.test = true;
-    var app = require('../../server')(config);
+    var app = require('../../app')(config);
     var server = app.listen(port);
     return server;
   }
